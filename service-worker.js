e0e18e4be624ee8459ff38def0a564961c8af6eb
siddek <siddek@ssd-tech.io>
2026-05-24 09:26:43 +0600

    PWA Final

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,21 @@
 const CACHE_NAME = 'electricity-bill-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/manifest.json',
-  '/pwa.js'
+    '/electricity-bill-app/',
+    '/electricity-bill-app/index.html',
+    '/electricity-bill-app/style.css',
+    '/electricity-bill-app/script.js',
+    '/electricity-bill-app/manifest.json'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-  self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+    self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    ))
-  );
-  self.clients.claim();
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
 });
